7566565a0129976e9624c7ffc56d17b5f569e3f3
chenshun131 <1539831174@qq.com>
2018-04-12 08:16:05 +0800

    commit some class

---
+++ pay-account/pay-account-provider/src/main/java/com/chenshun/account/provider/PayAccountProviderApplication.java
@@ -0,0 +1,13 @@
+package com.chenshun.account.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAccountProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayAccountProviderApplication.class, args);
+    }
+
+}
